29cdf966f23dc87e9942bab9abfdbfec36f352b8
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-09-22 18:26:37 -0300

    configurações iniciais do React

---
+++ nucont-test/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Essa vaga é minha!</h1>
+    </div>
+  );
+}
+
+export default App;
