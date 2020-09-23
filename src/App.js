b7f4741b3e41c412ffb5a0f1aee00999e4b2666c
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-09-23 15:44:08 -0300

    sem a tag no insdex

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import ReactGA from 'react-ga';
 import './App.css';
 import Home from './pages/Home';
 
+const GAInitial = () => {
+  ReactGA.initialize('UA-178751346-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 function App() {
-  useContext(() => ReactGA.initialize('UA-178751346-1'), []);
+  useContext(() => GAInitial(), []);
   return (
     <div className="App">
       <Home />
